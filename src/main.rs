47b03b15dfd983d70a8e4d8a0fe4063747c0d1c7
wareya <wareya@gmail.com>
2019-10-02 09:38:03 -0400

    simplify regex flavor, fix bug

--- src/main.rs
+++ src/main.rs
@@ -573,7 +573,7 @@ fn init() -> std::io::Result<ServerData>
     for mut c in "一｜丶ノ乙亅二亠人⺅𠆢儿入ハ丷冂冖冫几凵刀⺉力勹匕匚十卜卩厂厶又マ九ユ乃𠂉⻌口囗土士夂夕大女子宀寸小⺌尢尸屮山川巛工已巾干幺广廴廾弋弓ヨ彑彡彳⺖⺘⺡⺨⺾⻏⻖也亡及久⺹心戈戸手支攵文斗斤方无日曰月木欠止歹殳比毛氏气水火⺣爪父爻爿片牛犬⺭王元井勿尤五屯巴毋玄瓦甘生用田疋疒癶白皮皿目矛矢石示禸禾穴立⻂世巨冊母⺲牙瓜竹米糸缶羊羽而耒耳聿肉自至臼舌舟艮色虍虫血行衣西臣見角言谷豆豕豸貝赤走足身車辛辰酉釆里舛麦金長門隶隹雨青非奄岡免斉面革韭音頁風飛食首香品馬骨高髟鬥鬯鬲鬼竜韋魚鳥鹵鹿麻亀啇黄黒黍黹無歯黽鼎鼓鼠鼻齊龠".chars()
     {
         c = *data.radical_to_char.get(&c).unwrap_or(&c);
-        if data.char_to_comp.contains_key(&c)
+        if data.comp_to_char.contains_key(&c)
         {
             common_comps.insert(c, 0);
         }
@@ -582,7 +582,7 @@ fn init() -> std::io::Result<ServerData>
     for mut c in "电壴业㡀氺镸冋島烏甶耂龶允𧰨习七㐄肀𠂇予内央".chars() // 𠂭𠫔
     {
         c = *data.radical_to_char.get(&c).unwrap_or(&c);
-        if data.char_to_comp.contains_key(&c)
+        if data.comp_to_char.contains_key(&c)
         {
             common_comps.insert(c, 0);
         }
